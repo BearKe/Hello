24d7b29c95400a27aaec48d441964aa26d85b144
柯翔 <247329052@qq.com>
2017-03-16 15:52:52 +0800

    Initial Commit

---
+++ Hello Object-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Object-C
+//
+//  Created by KeX on 2017/3/16.
+//  Copyright © 2017年 KeX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
